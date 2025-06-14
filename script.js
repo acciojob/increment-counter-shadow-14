63f370787df9f44b20d71ad3731c348b92595682
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-14 17:38:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 let para = document.querySelector(".counter");
 let ibtn = document.querySelector(".incrementBtn");
 function fun(){
-	let count = parseInt(para.innerText)+1;
+	let count = parseInt(para.innerText);
 	alert(count);
-	para.innerText = count;
+	para.innerText = count+1;
 }
-
 ibtn.addEventListener("click",fun);
