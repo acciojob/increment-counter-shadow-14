223947087056c02a84b348d6150ae2c036e8cd65
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-14 17:29:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let para = document.querySelector(".counter");
+let ibtn = document.querySelector(".incrementBtn");
+function fun(){
+	let count = parseInt(para.innerText)+1;
+	para.innerText = count;
+}
+ibtn.addEventListener("click",fun);
